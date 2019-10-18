2ed002de74741d5c9655c0e9791379984655fe90
Dennis Padiernos <depadiernos@gmail.com>
2019-10-18 18:42:05 -0500

    initialize app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useReducer } from 'react';
+import TodoList from 'components/TodoList'
+
+function App() {
+    const 
+  return (
+    <div className="App">
+      <TodoList />
+      <AddTodo />
+    </div>
+  );
+}
+
+export default App;
