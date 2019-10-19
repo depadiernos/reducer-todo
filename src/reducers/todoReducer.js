dab1e742a3ddbda8729b84dfa3f783c6cdbe3624
Dennis Padiernos <depadiernos@gmail.com>
2019-10-18 20:31:20 -0500

    implement cases.

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,16 +1,25 @@
-export const initialState = [{
-    item: "This is my first todo task",
-    completed: false,
-    id: 1571438423
-}]
+export const initialState = [
+    {
+        item: "This is my first todo task",
+        completed: false,
+        id: 1571438423
+    },
+    {
+        item: "This is my second todo task",
+        completed: false,
+        id: 1571318423
+    }
+]
 
 export function todoReducer(state, action) {
     switch (action.type) {
         case "ADD_TODO":
-            return {...state};
-        case "REMOVE_TODO":
-            return {...state};
+            return [...state, action.payload];
+        case "REMOVE_COMPLETE":
+            return state.filter((item) => item.completed !== true);
         case "MARK_COMPLETE":
-            return {...state};
+            return state.map((item) => item.id === action.payload ? { ...item, completed: !item.completed } : item);
+        default:
+            return state;
     }
 }
